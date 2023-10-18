73740c29acd323ce6b929fafe7c62eb460e7ef1b
Nikitakorovinskii <kolbasa20022002@mail.ru>
2023-10-18 18:52:12 +0300

    ItLabsi

--- src/app.js
+++ src/app.js
@@ -13,12 +13,26 @@ if (!port) {
     throw new Error('PORT variable not set!');
 }
 const createdAt = new Date();
+const { Pool } = require('pg');
+const connectionString = 'postgresql://postgres:9090@db:5432/itLabs';
+const pool = new Pool({
+    connectionString: connectionString,
+});
+
 const server = http.createServer((req, res) => {
     fs.appendFileSync(logFilePath, `${new Date().toISOString()}:request\n`);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(`Hello World, started at ${createdAt.toISOString()}`);
+    pool.query('SELECT name FROM id', (err, result) => {
+        if (err) {
+            console.log('Error in query', err);
+            return;
+        }
+        res.end(`Result: ${result.rows[0].name}`);
+    });
+  //  res.end(`Hello World, started at ${createdAt.toISOString()}`);
 });
+
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
